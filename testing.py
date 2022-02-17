69b8efcfcdd878e7dc28db9de51cf856dc020cce
Corbin-Rochelle <ctr233@msstate.edu>
2022-02-17 10:32:54 -0600

    Beginning to Write in C

--- testing.py
+++ testing.py
@@ -1,19 +1,21 @@
 # Written by Corbin Rochelle
-import hybrid
-import quick_sort
-import selection_sort
+from hybrid import *
+from quick_sort import *
+from selection_sort import *
 import numpy
+import timeit
 
-startTest = 10
-choose = input("-------------\nWhich file do you wish to analyze?\n1. Quick Sort\n2. Selection Sort\n3. Hybrid\n-------------\n: ")
+startTest = 0
+choose = int(input("-------------\nWhich file do you wish to analyze?\n1. Quick Sort\n2. Selection Sort\n3. Hybrid\n-------------\n: "))
 
 if choose == 1:
-    for i in range(10):
-        startTest = startTest * i
+    for i in range(9):
+        startTest = startTest + 10
         arr = numpy.random.randint(1,101,startTest)
-        
+
         n = len(arr)
-        quickSort(arr, 0, n-1)
+        time = timeit.timeit(lambda: quickSort(arr, 0, n-1))
+        print(n, time)
 
 elif choose == 2:
     print("hello")
